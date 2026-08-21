5249a5008d70d59f8acb2c5c77acd4667223b4fc
fenna <fabianvanacoley@gmail.com>
2026-08-21 22:29:08 +0200

    quill + color scheme

---
+++ src/main/script.js
@@ -0,0 +1,30 @@
+import Quill from 'quill';
+
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+
+const quill = new Quill('#editor', {
+  theme: 'bubble',
+  placeholder: 'Start writing... I\'m sure it\'ll be great!',
+});
+
+
+body.classList.add('light-mode');
+
+
+
+function switch_color() {
+    const body = document.body;
+    
+    if (body.classList.contains('light-mode')) {
+        body.classList.replace('light-mode', 'dark-mode');
+        toggleButton.textContent = 'light';
+    } else {
+        body.classList.replace('dark-mode', 'light-mode');
+        toggleButton.textContent = 'dark';
+    }
+}
+
+toggleButton.addEventListener('click', switch_color);
+
